4f8a82f0cd306dcc3dafa3c687446fa88a582809
HeshamAbdallah02 <75794796+HeshamAbdallah02@users.noreply.github.com>
2025-04-28 23:00:52 +0300

    Installed @tailwindcss/container-queries to fix the issue with the container queries plugin not being found. 	modified:   package-lock.json 	modified:   package.json 	modified:   tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
-    ],
-    theme: {
-      extend: {
-        fontFamily: {
-          montserrat: ['Montserrat', 'sans-serif'],
-        },
-        fontSize: {
-          '7xl': ['4.5rem', { lineHeight: '1' }],     // 72px
-          '8xl': ['6rem', { lineHeight: '1' }],       // 96px
-          '9xl': ['8rem', { lineHeight: '1' }],       // 128px
-          'hero': ['10rem', { lineHeight: '1' }],     // 160px
-        },
-        transitionTimingFunction: {
-          'ease-out-quad': 'cubic-bezier(0.25, 0.46, 0.45, 0.94)',
-        },
-        containers: {
-          'splash': '20rem',
-        }
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
       },
+      fontSize: {
+        '7xl': ['4.5rem', { lineHeight: '1' }],
+        '8xl': ['6rem', { lineHeight: '1' }],
+        '9xl': ['8rem', { lineHeight: '1' }],
+        'hero': ['10rem', { lineHeight: '1' }],
+      },
+      transitionTimingFunction: {
+        'ease-out-quad': 'cubic-bezier(0.25, 0.46, 0.45, 0.94)',
+      }
     },
-    plugins: [
-      require('@tailwindcss/forms'),
-      require('@tailwindcss/container-queries'),
-    ],
-  }
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/container-queries'),
+  ],
+}
